6afac7f83d2b4a083552f78e985ee9cd67aeb805
Lipatova-Liza13 <lipatovaliza13@gmail.com>
2020-08-11 16:27:09 +0300

    Create project and add .gitignore

---
+++ ColectionView/ColectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColectionView
+//
+//  Created by Teslenko Anastasiya on 11.08.2020.
+//  Copyright © 2020 Liza Lipatova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
